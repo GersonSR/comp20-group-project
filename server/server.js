3c0f1fd1a45f4c000522cff4edbe2db65136cf6f
Chris Phifer <cphifer97@gmail.com>
2016-11-18 21:15:12 -0500

    Added more useful comments to server.js

--- server/server.js
+++ server/server.js
@@ -28,13 +28,62 @@ var db = mongo.connect(mongoUri, function (error, databaseConnection) {
 
 /* ***** Begin Route Definitions ***** */
 app.post('/submit', cors(), function(req, resp) {
-	/* POST route to submit scores to the global score database. */
-	/* Will include validation of the received scores so no 1337 h4x0rs cheat */
+	/* 
+	 * POST route to submit scores to the global score database. 
+	 * Will include validation of the received scores so no 1337 h4x0rs cheat 
+	 * Data will be structured in a JSON object (Mongo document) as:
+	 *
+	 * {
+	 * 		username: ,
+	 *		game-mode: ,
+	 *  	score: ,
+	 *		time: ,
+	 * }
+	 * 
+	 * Eventually the scores will be validated to prevent cheating. To do this,
+	 * we can employ some client-server encryption/decryption and comparison,
+	 * send different data to calculate scores on the server side, or do any
+	 * other of a number of things.
+	 *
+	 * In a later version we may change this data to contain not the raw score,
+	 * but Google Directions waypoints info to do all the calculations on the
+	 * server. This will make it very hard to cheat, as we're hiding our score
+	 * calculation algorithm.
+	 *
+	 * Usernames will be sanitized to prevent script-based attacks. Ditto for
+	 * game-modes. Times will be computed server side.
+	 *
+	 * game-mode MUST match a valid game mode, e.g. "easy", "hard", "local"...
+	 * If not, no data will be stored. Input validation FTW!
+	 */
+
 });
 
 app.get('/high-scores', cors(), function(req, resp) {
-	/* GET route to get back global high scores data available to everyone. */
-	/* Content returned in JSON format to be used by client to render nicely. */
+	/* 
+	 * GET route to get back global high scores data available to everyone. 
+	 * Content returned in JSON format to be used by client to render nicely. 
+	 * 
+	 * This route will handle sending back to the client global score data,
+	 * based on their selections on a form on the scores / statistics page.
+	 *
+	 * Data is sent back in JSON format for parsing (let the client do all
+	 * the nice looking stuff). This allows for a really simple model where
+	 * the client is responsible for all the tricky data handling to display
+	 * desired content.
+	 *
+	 * Validation on this route will be on form parameters: Sanitization and
+	 * comparison against a fixed set of valid inputs. This of course prevents
+	 * script-based attacks and only deals with data we want to be dealt with.
+	 *
+	 */
 });
 
+/*
+ * Future ideas: Routes to handle the generation of the start/end points
+ * to send to the client. This will facilitate better handling of the
+ * most important game mechanics, fewer errors due to client-side
+ * tomfoolery, and ultimately an experience where the client isn't so packed
+ * with stuff for the user to download and run locally.
+ */
 
