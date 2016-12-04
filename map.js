ce59abdcdd32f0648bfed83529e3ec4fc05edad0
asteph06 <ashtoncaulfieldstephens@gmail.com>
2016-12-03 22:11:29 -0500

    making function that properly finds two points within a certian distance of eachother

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+locations = {};
 var Lat = 0;
 var Lng = 0;
 var request
@@ -53,61 +54,102 @@ function renderMap() {
 		infowindow.open(map, marker);
 	});
 
-	radii = {
-		inner: 0,
-		outer: 1000
-	};
-
-	getRandomLocation( radii, client );
+	// read below
+	var two_places = locations.get_two_places( client, 10000, 0 );
 }
 
-// radii {
-// 		inner = inner_radius
-// 		outer = outer_radius
-// }
-function getRandomLocation( radii, LatLng ){
-
-	var request = {
-		radius: '' + radii.outer,
-		location: LatLng
-	};
-
-	service = new google.maps.places.PlacesService(map);
-	service.nearbySearch(request,randLocation_callback);
-}
-
-function randLocation_callback ( results, status ) {
-	/* 
-	// uncomment to show all the nearby places!!
-	//
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-	    	for (var i = 0; i < results.length; i++) {
-	      		console.log( results[i].name );
-	    	}
-	*/
-	  	var randy = getRandomInt( 0, results.length );
-	  	return results[randy];
-
-	  	// can determine wich of the results is in the boundry once
-	  	// LatLng is somehow added as a parameter
-	  	// 
-  		/*{  
-  		var i = 0;
-  		var dist  = google.maps.geometry.spherical.computeDistanceBetween ( results[i].geometry.location, LatLng );
-	    while( dist < radii.inner ) {
-	    	console.log( results[i].name );
-	    	i = (i+1)%results.length;
-	    	if( i == randy ){
-	    		console.log("no locations in between radii");
-	    		return;
-	    	}
-	    	dist = google.maps.geometry.spherical.computeDistanceBetween ( results[i].geometry.location, LatLng);
-		}
-		console.log(results[i].name);
-		return results[i];*/
+// SHOULD:
+// 		return a single object with two objects in it called place_1 and place_2
+// 		This will be held temporarily in locations.my_places
+// 			locations.my_places = {
+// 				place_1: (google place object),
+// 				place_2: (google place object)
+// 			}
+// 
+// WHAT IT DOES:
+// 		gets a center circle of radius maxDist, takes all places within that,
+// 		and compares all of them (starting at a random one) and then places 
+// 		those two into locations.my_places. When the function is called, the
+// 		locations object sets locations.my_places to 'undefined'. After the func
+// 		is called, the locations.my_places DOES BECOME TWO RANDOM PLACES.
+// 		
+// 		TODO: this function RETURN those two random places so dealing with ajax
+// 			  is easier.
+// 		
+// 		if there aren't any two places that fit the parameter specified, the
+// 		my_places object is set to 'no_two_places_satisfy_input' and to 
+// 		'google_status_error' if google returns a status error.
+// 		
+// keywords is optional
+locations.get_two_places = function( center, maxDist, minDist, keywords ) {
+	this.my_places  = undefined;
+	this.center     = center; 
+	this.minDist  	= minDist;
+	this.keywords  	= keywords;
+	this.get_all_locations_within( maxDist );
+	/*
+	while( this.my_places == undefined ){
+		// HOW DO YOU WAIT FOR SOMETHING IN JAVASCRIPT
 	}
+	return this.my_places;
+	*/
+};
+
+locations.get_all_locations_within = function( radius ){
+	var request = {
+		radius:   '' + radius,
+		location: this.center
+	};
+	if( this.keywords != undefined ){
+		request.keyword = this.keywords;
+	}
+	service = new google.maps.places.PlacesService(map);
+	service.nearbySearch( request, this.sort_response );
 }
 
+locations.sort_response = function( results, status ){	
+	if (status == google.maps.places.PlacesServiceStatus.OK) {
+		var randy   = getRandomInt( 0, results.length );
+		var index   = ( randy + 1 ) % results.length; 
+		var latlng1 = results[randy].geometry.location;
+		var latlng2;
+		var dist;
+    	while( results.length > 1  ){
+      		if ( index == randy ) {
+      			console.log
+      			results.splice( randy, 1 );
+      			randy  %= results.length;
+      			index   = (randy + 1) % results.length;
+      			latlng1 = results[randy].geometry.location;
+      		} else {
+      			latlng2 = results[index].geometry.location;
+      			dist    = google.maps.geometry.spherical.computeDistanceBetween( latlng1, latlng2 );
+      			// uncomment to see whats being compared
+				/* 
+      			console.log('\t '+ results[randy].name +
+      						'\t' + results[index].name +
+      						'\t' + dist) 
+      			*/
+      			if ( dist >= this.locations.minDist ) {
+      				this.locations.my_places = {
+      					place_1: results[randy], 
+      					place_2: results[index]
+      				};
+      				console.log(this.locations.my_places);				// console output
+      				return;
+      			}
+      			index = ( index + 1 ) % results.length;
+      		}
+    	}
+    	this.locations.my_places = 'no_two_places_satisfy_input';
+    	console.log(this.locations.my_places);							// console output
+      	return;
+    } else {
+    	this.locations.my_places = 'google_status_error';
+    	console.log(this.locations.my_places);							// console output
+    	return;
+    }
+}
 
 // callback must take in the parsed json object as a
 // a parameter.
