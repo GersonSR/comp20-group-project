6dc448082fd5deadb8c7b6381fc0c4e9ab8acc56
asteph06 <ashtoncaulfieldstephens@gmail.com>
2016-12-05 17:57:43 -0500

    making a flawless async function for finding places (yes that's a promise)

--- map.js
+++ map.js
@@ -54,45 +54,57 @@ function renderMap() {
 		infowindow.open(map, marker);
 	});
 
-	// read below
-	var two_places = locations.get_two_places( client, 10000, 0 );
+	//  !!
+	EXAMPLE();
+	//  !!
 }
 
-// SHOULD:
-// 		return a single object with two objects in it called place_1 and place_2
-// 		This will be held temporarily in locations.my_places
-// 			locations.my_places = {
-// 				place_1: (google place object),
-// 				place_2: (google place object)
-// 			}
+//TEMPORARY!!!!!!!1 
+// example on how to use locations.get_two_places!
+function EXAMPLE(){
+	var promise_object = locations.get_two_places( client, 10000, 0 )
+	.then( function(some_places){
+		console.log('these are some places!');
+		console.log(JSON.stringify(some_places,null,4)); 
+	}, function(some_error){
+		console.log('this is where some_error would go!');
+		console.log(some_error);
+	});
+
+	/* promise_object.then(
+	 * 		~function to handle success~,
+	 * 	 	~function to handle error  ~
+	 * 	);
+	 */
+
+	console.log('this is a promise object');
+	console.log( promise_object );
+	// printed before other things ^^
+}
+
+// returns a promise object that will resolve with places and throw
+// an error if something goes wrong.
 // 
-// WHAT IT DOES:
-// 		gets a center circle of radius maxDist, takes all places within that,
-// 		and compares all of them (starting at a random one) and then places 
-// 		those two into locations.my_places. When the function is called, the
-// 		locations object sets locations.my_places to 'undefined'. After the func
-// 		is called, the locations.my_places DOES BECOME TWO RANDOM PLACES.
-// 		
-// 		TODO: this function RETURN those two random places so dealing with ajax
-// 			  is easier.
-// 		
-// 		if there aren't any two places that fit the parameter specified, the
-// 		my_places object is set to 'no_two_places_satisfy_input' and to 
-// 		'google_status_error' if google returns a status error.
-// 		
-// keywords is optional
+// resolve format: {
+// 						place_1: (google place object)
+// 						place_2: (google place object)
+// 						dist:    (distance in meters )
+// 				   }
+//  
+//  all units of distance are in meters
 locations.get_two_places = function( center, maxDist, minDist, keywords ) {
-	this.my_places  = undefined;
-	this.center     = center; 
-	this.minDist  	= minDist;
-	this.keywords  	= keywords;
-	this.get_all_locations_within( maxDist );
-	/*
-	while( this.my_places == undefined ){
-		// HOW DO YOU WAIT FOR SOMETHING IN JAVASCRIPT
-	}
-	return this.my_places;
-	*/
+	return new Promise((resolve,reject)=>{
+		this.my_places  = 'loading';
+		this.center     = center; 
+		this.minDist  	= minDist;
+		this.keywords  	= keywords;
+		this.get_all_locations_within( maxDist )
+		.then(locations.sort_response)
+		.then(
+			(good )=>{					 resolve( good );},
+			(error)=>{console.log(error);reject( error );}
+		);
+	});
 };
 
 locations.get_all_locations_within = function( radius ){
@@ -103,52 +115,63 @@ locations.get_all_locations_within = function( radius ){
 	if( this.keywords != undefined ){
 		request.keyword = this.keywords;
 	}
-	service = new google.maps.places.PlacesService(map);
-	service.nearbySearch( request, this.sort_response );
+	return new Promise( function( resolve, reject ){ 
+		service = new google.maps.places.PlacesService(map);
+		service.nearbySearch( request, function( results, status ) {
+			if ( status == google.maps.places.PlacesServiceStatus.OK) {
+				resolve( results );
+				return;
+			}else {
+				reject( 'google_status_error' );
+				return;
+			}	
+		});
+	});
 }
 
-locations.sort_response = function( results, status ){	
-	if (status == google.maps.places.PlacesServiceStatus.OK) {
+locations.sort_response = function( results ){	
+	return new Promise((resolve, reject)=>{
 		var randy   = getRandomInt( 0, results.length );
 		var index   = ( randy + 1 ) % results.length; 
 		var latlng1 = results[randy].geometry.location;
 		var latlng2;
 		var dist;
-    	while( results.length > 1  ){
-      		if ( index == randy ) {
-      			console.log
-      			results.splice( randy, 1 );
-      			randy  %= results.length;
-      			index   = (randy + 1) % results.length;
-      			latlng1 = results[randy].geometry.location;
-      		} else {
-      			latlng2 = results[index].geometry.location;
-      			dist    = google.maps.geometry.spherical.computeDistanceBetween( latlng1, latlng2 );
-      			// uncomment to see whats being compared
+		while( results.length > 1  ){
+	  		if ( index == randy ) {
+	  			console.log
+	  			results.splice( randy, 1 );
+	  			randy  %= results.length;
+	  			index   = (randy + 1) % results.length;
+	  			latlng1 = results[randy].geometry.location;
+	  		} else {
+	  			latlng2 = results[index].geometry.location;
+	  			dist    = google.maps.geometry.spherical.computeDistanceBetween( latlng1, latlng2 );
+	  			// uncomment to see whats being compared and how far they are from eachother
 				/* 
-      			console.log('\t '+ results[randy].name +
-      						'\t' + results[index].name +
-      						'\t' + dist) 
-      			*/
-      			if ( dist >= this.locations.minDist ) {
-      				this.locations.my_places = {
-      					place_1: results[randy], 
-      					place_2: results[index]
-      				};
-      				console.log(this.locations.my_places);				// console output
-      				return;
-      			}
-      			index = ( index + 1 ) % results.length;
-      		}
-    	}
-    	this.locations.my_places = 'no_two_places_satisfy_input';
-    	console.log(this.locations.my_places);							// console output
-      	return;
-    } else {
-    	this.locations.my_places = 'google_status_error';
-    	console.log(this.locations.my_places);							// console output
-    	return;
-    }
+	  			console.log('\t '+ results[randy].name +
+	  						'\t' + results[index].name +
+	  						'\t' + dist) 
+	  			*/
+	  			if ( dist >= this.locations.minDist ) {
+	  				this.locations.my_places = {
+	  					place_1: results[randy], 
+	  					place_2: results[index],
+	  					dist: dist
+	  				};
+	  				/*
+	  				// uncomment too see the names of resulting places 
+	  				console.log(this.locations.my_places.place_1.name); 
+	  				console.log(this.locations.my_places.place_2.name);
+	  				*/
+	  				resolve( this.locations.my_places );
+	  				return;
+	  			}
+	  			index = ( index + 1 ) % results.length;
+	  		}
+		}
+	  	reject('no_two_places_satisfy_input');
+	  	return;
+	  });
 }
 
 // callback must take in the parsed json object as a
